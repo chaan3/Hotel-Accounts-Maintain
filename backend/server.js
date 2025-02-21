6b886433c37f0a5b3ca9047fb0e76d017ae3b0f9
chaan3 <chandaji628@gmail.com>
2025-02-21 10:14:02 +0530

    server.js update

--- backend/server.js
+++ backend/server.js
@@ -261,7 +261,7 @@ app.post("/verify", async (req, res) => {
 
 
 // Server setup
-const port =3000;
+const port =process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
